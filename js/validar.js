053e083ca4c02f2073f8747d01dbf2735e309f82
17065022@alumnos.icel.edu.mx <17065022@alumnos.icel.edu.mx>
2020-06-26 22:33:11 -0500

    creacion de funciones

--- js/validar.js
+++ js/validar.js
@@ -1,17 +1,53 @@
-function validarn(e)
-{
-    var teclado = (document.all)? e.keycode: e.witch
-    if (teclado==8)return true;
-    var patron=/[0-9\d .]/;
-    var tec = string.fromcharcode(teclado);
-    return patron.test(tec);
+function longitudynumeros(e,contenidovalor, longitudMaxima){
+    try {
+        if (teclado==8||contenidovalor.value.length > (longitudMaxima - 1))
+            return false;
+        else
+        var patron = /[0-9\d .]/;
+        var tec = String.fromCharCode(teclado);
+        return patron.test(tec);             
+        } 
+        catch (e) {
+        return false;
+    }
+
 }
-function interes ()
+function validarlongitud( contenidovalor, longitudMaxima)
 {
+    try {
+        if (contenidovalor.value.length > (longitudMaxima - 1))
+            return false;
+        else
+            return true;             
+        } 
+        catch (e) {
+        return false;
+    }
+}
+function validar(e,contenido)
+{
+    
+            
+    var teclado = (document.all) ? e.keycode: e.which
+    
+    if (teclado==8 && contenido.value.length<12)return true;
+    
+    
+    var patron = /[0-9\d .]/;
+    var tec = String.fromCharCode(teclado);
+    console.log(tec);
+    
+    return patron.test(tec);
+    
+   
+}
+function interes()
+{
+    
     var valor=document.formulario.cantidad.value;
     var result = parseInt(valor);
     var interes= result * 0.02;
-    var total=result + interes;
+    var total = result + interes;
     document.formulario.sueldoti.value = "$" + total;
 }
 
